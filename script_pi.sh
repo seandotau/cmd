9a40f7a2a6c00d5c2dde278995cd2a3a005106c5
seandotau <seandotau@gmail.com>
2023-07-09 17:49:03 +1000

    Update script_pi.sh

--- script_pi.sh
+++ script_pi.sh
@@ -5,6 +5,7 @@ node -v
 npm -v
 python3 --version
 pip -V
+git --version
 
 # Install Node.js
 sudo curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
@@ -16,8 +17,12 @@ sudo apt install -y python3
 # Install PIP
 sudo apt install -y python3-pip
 
+# Install Git
+sudo apt install -y git
+
 # Verify installations
 node -v
 npm -v
 python3 --version
 pip -V
+git --version
