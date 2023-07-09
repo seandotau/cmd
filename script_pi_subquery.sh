78ded10646bc5725a071c008ad881488be4ed3e2
seandotau <seandotau@gmail.com>
2023-07-09 17:18:12 +1000

    Update script_pi_subquery.sh

--- script_pi_subquery.sh
+++ script_pi_subquery.sh
@@ -2,9 +2,13 @@
 
 # Check installations
 subql -v
+subql-node --version
 
 # Install Node.js
 sudo npm install -g @subql/cli
+sudo npm install -g @subql/node
+sudo npm install -g @subql/query
 
 # Verify installations
 subql -v
+subql-node --version
